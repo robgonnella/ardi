d43af945656774cff3ff87ff16871953e429a06f
Rob Gonnella <robgonnella@gmail.com>
2020-06-13 08:53:08 -0700

    Update package comment

--- v2/main.go
+++ v2/main.go
@@ -6,12 +6,17 @@ process.
 Things ardi can fo for you:
 
 • Manage versioned platforms and libraries on a per-project basis
-• Store user defined build configurations with a mechanism for easily running
-  consistent and repeatable builds.
+
+• Store user defined build config for consistent and repeatable builds.
+
 • Enable running your builds in a CI pipeline
+
 • Compile and upload to an auto discovered connected board
-• Watche a sketch for changes and auto recompil / reupload to a connected board
+
+• Watch sketch for changes and auto recompile / reupload
+
 • Print various info about platforms and boards
+
 • Search and print available libraries and versions
 
 Ardi should work for all boards and platforms supported by arduino-cli.
