07a70bbbdbc37301ef05eeeec55d9f103f5ba9f6
Rob Gonnella <robgonnella@gmail.com>
2020-07-11 20:49:46 -0400

    Add end-to-end test script and update docs

---
+++ v2/scripts/run_e2e.sh
@@ -0,0 +1,28 @@
+set -euox pipefail
+
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function clean_up {
+  rm -rf $here/../test_projects/pixie/build
+  rm -rf $here/../.ardi $here/../ardi.json
+}
+
+trap "clean_up" EXIT
+
+clean_up
+
+GO111MODULE=on go get github.com/robgonnella/ardi/v2
+
+ardi project init -v
+
+ardi project add platforms --all -v
+
+ardi project add lib "Adafruit Pixie" -v
+
+ardi project add build \
+  --name pixie \
+  --platform arduino:avr \
+  --fqbn arduino:avr:mega \
+  --sketch $here/../test_projects/pixie
+
+ardi project build pixie
