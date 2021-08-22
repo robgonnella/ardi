f7f6314c4412b96080697d80bf7967ffd1d3ed29
Rob Gonnella <robgonnella@gmail.com>
2021-08-22 15:56:16 -0400

    Improve help command

--- v2/commands/help.go
+++ v2/commands/help.go
@@ -23,10 +23,32 @@ func Help(cmd *cobra.Command, args []string) {
 	}
 
 	fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Long, wrapLen))
-	fmt.Printf("Usage:\n")
-	fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Use, wrapLen))
-	fmt.Printf("Flags:\n")
-	fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Flags().FlagUsages(), wrapLen))
-	fmt.Printf("Global Flags:\n")
-	fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Root().Flags().FlagUsages(), wrapLen))
+	fmt.Println("Usage:")
+	if cmd.Runnable() {
+		fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.CommandPath(), wrapLen))
+	}
+	if cmd.HasSubCommands() {
+		use := cmd.CommandPath() + " [command]"
+		fmt.Printf("%s\n\n", wordwrap.WrapString(use, wrapLen))
+	}
+	if cmd.HasSubCommands() {
+		fmt.Println("Available Commands:")
+		for _, c := range cmd.Commands() {
+			line := fmt.Sprintf("%-*s %s", c.NamePadding(), c.Name(), c.Short)
+			fmt.Println(wordwrap.WrapString(line, wrapLen))
+		}
+		fmt.Println("")
+	}
+	if cmd.HasFlags() {
+		fmt.Printf("Flags:\n")
+		fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Flags().FlagUsages(), wrapLen))
+	}
+	if cmd.Root().HasFlags() {
+		fmt.Printf("Global Flags:\n")
+		fmt.Printf("%s\n\n", wordwrap.WrapString(cmd.Root().Flags().FlagUsages(), wrapLen))
+	}
+	if cmd.HasSubCommands() {
+		fmt.Println("Use \"ardi [command] --help\" for more information about a command.")
+	}
+	fmt.Println("")
 }
